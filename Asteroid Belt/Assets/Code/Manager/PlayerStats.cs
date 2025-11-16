591128b586d3e3472ad7c427538d77235277b344
MajorNimrod <house.j2024@gmail.com>
2025-11-15 17:52:05 -0800

    Added gamemanager functionality and playerstats

---
+++ Asteroid Belt/Assets/Code/Manager/PlayerStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PlayerStats
+{
+    public int maxHealth;
+    public float moveSpeed;
+    public float damage;
+    public float fireRate;
+}
